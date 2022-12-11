58a623754b835c5054f2ab77db49e4e43f3ba744
Nikita <nikita@oden.com>
2022-12-12 01:14:12 +0300

    Add the two event listeners

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const digits = document.querySelectorAll('.digit');
+const operator = document.querySelectorAll('.operator');
 
+
+digits.forEach(e => {
+    e.addEventListener('click', e => updateDisplay(e.target.textContent));
+})
+operator.forEach(e => {
+    e.addEventListener('click', e => operate(e.target.textContent));
+});
 
 
 const add = (a, b) => a + b;
@@ -7,6 +15,39 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-function operate(operator, a, b) {
+let currentOperand = 0;
+let previousOperand = null;
+
+
+function operate(operator) {
+    switch (operator) {
+        case '+':
+            add();
+            break;
+        case '-':
+            subtract();
+            break;
+        case 'x':
+            multiply();
+            break;
+        case '÷':
+            divide();
+            break;
+        case '=':
+
+            break;
+        case '.':
+            break;
+
+        case 'C':
+
+            break;
+        case 'AC':
+
+            break;
+    }
+}
+
+function updateDisplay(digit){
 
 }
